9c397baa6a983ab30a12eb165c492d0967271f0b
Trigger021 <enoschege@gmail.com>
2024-03-26 13:05:20 +0300

    ...

--- app/src/main/java/com/example/chegeapp/IntentsActivity.kt
+++ app/src/main/java/com/example/chegeapp/IntentsActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -37,8 +38,12 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.core.net.toUri
 import com.example.chegeapp.ui.theme.ChegeAppTheme
 
@@ -213,7 +218,19 @@ fun MyIntents(){
         }
         //End of button
         Spacer(modifier = Modifier.height(20.dp))
-
+        Text(
+            text = "Do you have an Account? if you dont Register",
+            fontSize = 20.sp,
+            modifier = Modifier
+                .fillMaxWidth()
+                .clickable {
+                    mContext.startActivity(Intent(mContext,formactivity::class.java))
+                },
+            textAlign = TextAlign.Center,
+            fontWeight = FontWeight.Bold,
+            fontFamily = FontFamily.Cursive,
+            color = Color.White
+        )
     }
 }
 @Preview(showBackground = true)
---
+++ app/src/main/java/com/example/chegeapp/formactivity.kt
@@ -0,0 +1,160 @@
+package com.example.chegeapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Phone
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.paint
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.chegeapp.ui.theme.ChegeAppTheme
+
+class formactivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyForm()
+        }
+    }
+}
+
+@Composable
+fun MyForm(){
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+            .paint(
+                painterResource(id = R.drawable.backgrounds),
+                contentScale = ContentScale.FillBounds
+            )
+    )
+    {
+        var firstname by remember { mutableStateOf("") }
+        var lastname by remember { mutableStateOf("") }
+        var email by remember { mutableStateOf("") }
+        var password by remember { mutableStateOf("") }
+        var contact by remember { mutableStateOf("") }
+        Box(
+            modifier = Modifier.fillMaxWidth(),
+            contentAlignment = Alignment.Center
+        ){
+            Image(
+                painter = painterResource(id = R.drawable.signup),
+                contentDescription = "yeah",
+                modifier = Modifier.size(180.dp),
+
+
+            )
+        }
+        Text(
+            text = "CREATE AN ACCOUNT",
+            fontSize = 20.sp,
+            modifier = Modifier
+                .fillMaxWidth(),
+            textAlign = TextAlign.Center,
+            fontWeight = FontWeight.Bold,
+            fontFamily = FontFamily.Cursive,
+            color = Color.White
+        )
+        Spacer(modifier = Modifier.height(10.dp))
+        OutlinedTextField(
+            value = firstname,
+            onValueChange = {firstname = it},
+            placeholder = { Text(text = "Firstname")},
+            leadingIcon = { Icon(imageVector = Icons.Default.Person, contentDescription = "person")},
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 20.dp, end = 20.dp),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text)
+        )
+        Spacer(modifier = Modifier.height(10.dp))
+        OutlinedTextField(
+            value = lastname,
+            onValueChange = {lastname = it},
+            placeholder = { Text(text = "Lastname")},
+            leadingIcon = { Icon(imageVector = Icons.Default.Person, contentDescription = "person")},
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 20.dp, end = 20.dp),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text)
+        )
+        Spacer(modifier = Modifier.height(10.dp))
+        OutlinedTextField(
+            value = email,
+            onValueChange = {email = it},
+            placeholder = { Text(text = "Email")},
+            leadingIcon = { Icon(imageVector = Icons.Default.Email, contentDescription = "email")},
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 20.dp, end = 20.dp),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email)
+        )
+        Spacer(modifier = Modifier.height(10.dp))
+        OutlinedTextField(
+            value = password,
+            onValueChange = {password = it},
+            placeholder = { Text(text = "Password")},
+            leadingIcon = { Icon(imageVector = Icons.Default.Lock, contentDescription = "password")},
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 20.dp, end = 20.dp),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+            visualTransformation = PasswordVisualTransformation()
+        )
+        Spacer(modifier = Modifier.height(10.dp))
+        OutlinedTextField(
+            value = contact,
+            onValueChange = {contact = it},
+            placeholder = { Text(text = "Phone Number")},
+            leadingIcon = { Icon(imageVector = Icons.Default.Phone, contentDescription = "contact")},
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 20.dp, end = 20.dp),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+        )
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MyFormPreview(){
+    MyForm()
+}
